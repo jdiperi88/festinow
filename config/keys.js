0fbea88bc690ece7656080d4e0c9e933988fc49d
asher978 <ashershaheen@gmail.com>
2018-02-11 17:29:13 -0500

    updated mongoURI to run on my local machine

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,6 @@
 module.exports = {
    googleClientID: '230803788016-mcdi1agsvt22k9tqrs8vhljl68htbjea.apps.googleusercontent.com',
    googleClientSecret: "LVJwU-gDinC8P8fhefspIaT9",
-   mongoURI: 'mongodb://jdiperi88:password@ds121588.mlab.com:21588/email-dev'
+   mongoURI: 'mongodb://localhost:27017/festival'
+  //  mongoURI: 'mongodb://jdiperi88:password@ds121588.mlab.com:21588/email-dev'
 }
