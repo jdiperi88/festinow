d34d632298eb352dad88588b63ced5c29a45e268
asher978 <ashershaheen@gmail.com>
2018-02-11 21:38:51 -0500

    added mlab keys

--- config/keys.js
+++ config/keys.js
@@ -1,6 +1,13 @@
 module.exports = {
+   googleClientID: '130300529398-4rcl87pslfh7oam446plttb43ku9pgjr.apps.googleusercontent.com',
+   googleClientSecret: 'hNrV36LIL_OBNoRLGBd1bsl6',
+   mongoURI: 'mongodb://ash:password@ds131698.mlab.com:31698/festival-dev',
+   cookieKey: 'kjhsdfjhsfjhskjfhjkshfgdfjghdfkjghfjkdhgkjdfhgkjhdfgkjhdfjgkhiuywerwe'
+
+   /* <------JOEYS SETTINGS----->
    googleClientID: '230803788016-mcdi1agsvt22k9tqrs8vhljl68htbjea.apps.googleusercontent.com',
    googleClientSecret: "LVJwU-gDinC8P8fhefspIaT9",
-   mongoURI: 'mongodb://localhost:27017/festival'
-  //  mongoURI: 'mongodb://jdiperi88:password@ds121588.mlab.com:21588/email-dev'
+   mongoURI: 'mongodb://jdiperi88:password@ds121588.mlab.com:21588/email-dev'
+   */
+   
 }
