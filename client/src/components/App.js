727c282cbec74540fd48ffafb36dfa44ca09dd04
asher978 <ashershaheen@gmail.com>
2018-02-12 18:41:14 -0500

    react router setup

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
+import Header from './Header';
+import Landing from './Landing';
+import Dashboard from './Dashboard';
 
 const App = () => {
   return (
     <div>
-      Festival App
+      <Router>
+        <div>
+          <Header />  
+          <Route exact path="/" component={Landing} />
+          <Route exact path="/dash" component={Dashboard} />
+        </div>
+      </Router>
     </div>
   );
 };
